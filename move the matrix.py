2ee45f49b818e582139c65c72b995f8ca1e1dfb7
Wan-Yifei <wanyifei0123@126.com>
2018-05-05 00:55:12 -0500

    feat: Add the function of tracking pathway
    Add the code to allow the function move and track the pathway of points
    which need to be moved.
    Line: #50~#58

--- move the matrix.py
+++ move the matrix.py
@@ -12,9 +12,9 @@ eg: M1: ([0,0,0], [0,1,0], [1,1,0])  M2([0,1,1], [0,1,1], [0,0,0]);
 import numpy as np
 
 #1. input matrix and reshape
-#m1, m2 = np.array([[0,0,0], [0,1,0], [1,1,0]]), np.array([[0,1,1], [0,1,1], [0,0,0]])
+m1, m2 = np.array([[0,0,0], [0,1,0], [1,1,0]]), np.array([[0,1,1], [0,1,1], [0,0,0]])
 #m1, m2 = np.array([[0,0,0,1], [0,1,0,0], [1,1,0,0]]), np.array([[0,1,0,1], [0,1,1,0], [0,0,0,0]])
-m1, m2 = np.random.binomial(1, 0.5, size = [10, 10]), np.random.binomial(1, 0.3, size = [10, 10])
+#m1, m2 = np.random.binomial(1, 0.5, size = [20, 20]), np.random.binomial(1, 0.3, size = [20, 20])
 
 def move(m1, m2):
     shape = m1.shape
@@ -37,13 +37,26 @@ def move(m1, m2):
     start.sort()
     target.sort()
 
-         
-    try:                                      ## moving via exchange points
+    #3. moving via exchange points     
+    try:                                      
         k = 0
+        pathway = {}                         ## dict for pathway
         for i in start:
+            ## move the point
             temp = m1[i]
             m1[i] = m1[target[k]]
             m1[target[k]] = temp
+            ## compute the pathway
+            dist = i - target[k]
+            if dist < 0:
+                vertical = abs(dist)//shape[1]
+                horizon = abs(dist)%shape[1]
+                pathway[i] = pathway.get(i, [vertical, horizon])
+            else:
+                vertical = -abs(dist)//shape[1]
+                horizon = -(abs(dist)%shape[1])
+                pathway[i] = pathway.get(i, [vertical, horizon])
+                
             k = k + 1
     except IndexError:
         print('length of start: %d and target: %d'%(len(start),len(target)))
@@ -51,23 +64,25 @@ def move(m1, m2):
     else:
         print('Moving is completed')
     
-# =============================================================================
-#     #double check the result:    
-#     ind1 = list(np.where(m1 == 1))[0]           
-#     ind2 = list(np.where(m2 == 1))[0]
-#     set1, set2 = set(ind1), set(ind2)
-#     check = len(set1 & set2)
-#     print(check == n)   
-# =============================================================================
+    #double check the result:    
+    ind1 = list(np.where(m1 == 1))[0]           
+    ind2 = list(np.where(m2 == 1))[0]
+    set1, set2 = set(ind1), set(ind2)
+    check = len(set1 & set2)
+    print(check == n)   
+    
     finalm = m1.reshape(shape, order = 'F')
     origm = m2.reshape(shape, order = 'F')
+    
+    start
+    
     print('Most match of 1 is %d'%(n))
     print('\n')
     #print(m1f)
     print(finalm)
     print('\n')
     print(origm)
-    return finalm, origm
+    return pathway
 
-move(m1,m2)
+pathway = move(m1,m2)
     
